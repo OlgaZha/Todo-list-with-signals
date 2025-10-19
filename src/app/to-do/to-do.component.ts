8d96a0a0af80393a3df130cd194ed91bd9381e33
OlgaZha <zhaldakolga484@gmail.com>
2025-10-19 19:05:46 +0200

    add filtering, save and load local storage, implement deletion

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,28 +1,68 @@
 import {Component, computed, effect, signal} from '@angular/core';
-import {NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+
+interface Todo {
+  text: string;
+  isCompleted: boolean;
+}
 
 @Component({
   selector: 'app-to-do',
   imports: [
-    NgIf
+    FormsModule,
+    NgForOf
   ],
   templateUrl: './to-do.component.html',
   styleUrl: './to-do.component.scss'
 })
+
 export class ToDoComponent {
-  todos = signal<string[]>([]);
-  remainingCount = computed(() => this.todos().length);
-  newTodo = '';
+  todos = signal<Todo[]>([]);
+  newTodo: Todo = {
+    text: '',
+    isCompleted: false,
+  };
+  filter = signal<'all' | 'active' | 'completed'>('all')
+  remainingCount  = computed(() => this.todos().filter(todo => !todo.isCompleted));
+  completedCount = computed(() => this.todos().filter(todo => todo.isCompleted));
+  filteredTodo =  computed(() => {
+    switch(this.filter()) {
+      case 'active': return this.todos().filter(todo => !todo.isCompleted);
+      case 'completed': return this.todos().filter(todo => todo.isCompleted);
+      default: return this.todos();
+    }
+  })
+
   constructor() {
-    effect(() => console.log(this.todos()))
+    effect(() => {
+      localStorage.setItem('todos', JSON.stringify(this.todos()));
+    })
+    const localTodos = JSON.parse(localStorage.getItem('todos') || '[]') as Todo[];
+    if(localTodos.length > 0) {
+      this.todos.update(() => localTodos);
+    }
   }
 
   addToDo() {
     this.todos.update(list => [...list, this.newTodo]);
-    this.newTodo = '';
+    this.newTodo = {
+      text: '',
+      isCompleted: false,
+    };
   }
+
+  updateTodoStatus(todoId: number){
+    this.todos.update(todos => {
+      todos[todoId].isCompleted = !todos[todoId].isCompleted;
+      return [...todos];
+    })
+  }
+
   removeToDo(inputIndex: number): void {
     this.todos.update(list => list.filter((element, index) => index !== inputIndex));
   }
-
+  clearAll(): void {
+    this.todos.set([])
+  }
 }
