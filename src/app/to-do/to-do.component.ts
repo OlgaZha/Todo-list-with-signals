48e251d3fba598f1a76637e899e2dab7ed3d72a8
OlgaZha <zhaldakolga484@gmail.com>
2025-11-01 10:18:40 +0100

    using signal in form

--- src/app/to-do/to-do.component.ts
+++ src/app/to-do/to-do.component.ts
@@ -1,68 +1,37 @@
 import {Component, computed, effect, signal} from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgForOf} from '@angular/common';
-
-interface Todo {
-  text: string;
-  isCompleted: boolean;
-}
+import {Todo, TodoServiceService} from '../todo-service.service';
 
 @Component({
   selector: 'app-to-do',
   imports: [
     FormsModule,
-    NgForOf
+    NgForOf,
+    ReactiveFormsModule,
   ],
   templateUrl: './to-do.component.html',
   styleUrl: './to-do.component.scss'
 })
 
 export class ToDoComponent {
-  todos = signal<Todo[]>([]);
   newTodo: Todo = {
     text: '',
     isCompleted: false,
   };
-  filter = signal<'all' | 'active' | 'completed'>('all')
-  remainingCount  = computed(() => this.todos().filter(todo => !todo.isCompleted));
-  completedCount = computed(() => this.todos().filter(todo => todo.isCompleted));
-  filteredTodo =  computed(() => {
-    switch(this.filter()) {
-      case 'active': return this.todos().filter(todo => !todo.isCompleted);
-      case 'completed': return this.todos().filter(todo => todo.isCompleted);
-      default: return this.todos();
-    }
-  })
-
-  constructor() {
-    effect(() => {
-      localStorage.setItem('todos', JSON.stringify(this.todos()));
-    })
-    const localTodos = JSON.parse(localStorage.getItem('todos') || '[]') as Todo[];
-    if(localTodos.length > 0) {
-      this.todos.update(() => localTodos);
-    }
+  constructor(public todoService: TodoServiceService) {
   }
 
-  addToDo() {
-    this.todos.update(list => [...list, this.newTodo]);
+  addTodo() {
+    this.todoService.addToDo(this.newTodo)
     this.newTodo = {
       text: '',
       isCompleted: false,
     };
   }
 
-  updateTodoStatus(todoId: number){
-    this.todos.update(todos => {
-      todos[todoId].isCompleted = !todos[todoId].isCompleted;
-      return [...todos];
-    })
+  onSubmitForm() {
+
   }
 
-  removeToDo(inputIndex: number): void {
-    this.todos.update(list => list.filter((element, index) => index !== inputIndex));
-  }
-  clearAll(): void {
-    this.todos.set([])
-  }
 }
---
+++ src/app/todo-service.service.ts
@@ -0,0 +1,78 @@
+import {computed, effect, Injectable, Signal, signal} from '@angular/core';
+import {FormBuilder, FormControl, FormControlStatus, FormGroup, Validators} from '@angular/forms';
+import {toSignal} from '@angular/core/rxjs-interop';
+
+export interface Todo {
+  text: string;
+  isCompleted: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoServiceService {
+  private todos = signal<Todo[]>([]);
+  externalTodos = this.todos.asReadonly();
+  filter = signal<'all' | 'active' | 'completed'>('all')
+  remainingCount  = computed(() => this.todos().filter(todo => !todo.isCompleted));
+  completedCount = computed(() => this.todos().filter(todo => todo.isCompleted));
+  filteredTodo =  computed(() => {
+    switch(this.filter()) {
+      case 'active': return this.todos().filter(todo => !todo.isCompleted);
+      case 'completed': return this.todos().filter(todo => todo.isCompleted);
+      default: return this.todos();
+    }
+  })
+  readonly profileForm: FormGroup;
+  readonly nameSignal: Signal<string>;
+  readonly emailSignal: Signal<string>;
+  readonly profileStatusSignal: Signal<FormControlStatus>;
+  constructor(private fb: FormBuilder) {
+    effect(() => {
+      localStorage.setItem('todos', JSON.stringify(this.todos()));
+    })
+    const localTodos = JSON.parse(localStorage.getItem('todos') || '[]') as Todo[];
+    if(localTodos.length > 0) {
+      this.todos.update(() => localTodos);
+    }
+
+    this.profileForm = this.buildProfileForm();
+    this.nameSignal = this.toSignalWithInitial('name');
+    this.emailSignal = this.toSignalWithInitial('email');
+    this.profileStatusSignal = toSignal(this.profileForm.statusChanges, {initialValue: this.profileForm.status})
+  }
+
+  addToDo(newTodo: Todo) {
+    this.todos.update(list => [...list, newTodo]);
+  }
+
+  updateTodoStatus(todo: Todo){
+    this.todos.update(todos => {
+      todo.isCompleted = !todo.isCompleted;
+      return [...todos];
+    })
+  }
+
+  setFilter(value: any) {
+    this.filter.set(value)
+  }
+
+  removeToDo(inputIndex: number): void {
+    this.todos.update(list => list.filter((element, index) => index !== inputIndex));
+  }
+  clearAll(): void {
+    this.todos.set([])
+  }
+
+  buildProfileForm(): FormGroup {
+    return this.fb.group({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    })
+  }
+
+  toSignalWithInitial(controlName: string): Signal<string> {
+    let control = this.profileForm.get(controlName)!;
+    return toSignal(control.valueChanges, {initialValue: control.value});
+  }
+}
