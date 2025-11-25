38a9511e2bef7029b70bed3c1021a4635df6f432
OlgaZha <zhaldakolga484@gmail.com>
2025-11-25 20:05:02 +0100

    Adding the possibility to add subtasks part 2

--- src/app/todo-quotes/todo-quotes.component.ts
+++ src/app/todo-quotes/todo-quotes.component.ts
@@ -10,27 +10,24 @@ import {TodoServiceService} from '../todo-service.service';
 export class TodoQuotesComponent {
   todoService = inject(TodoServiceService)
   constructor() {
-    effect(() => {
-      if(this.todoService.motivationEvents() > 0) {
-        this.showRandomQuote()
-      }
-    });
+    this.showRandomQuote()
   }
-  randomQuote = signal<string>('Don’t wait for opportunity — create it.')
+
+  randomQuote = signal<string>('')
   quotes = [
       "Success doesn’t come from what you do occasionally, it comes from what you do consistently.",
       "Discipline is choosing what you want most over what you want now.",
       "Your only limit is the amount of action you’re willing to take.",
-      "",
       "Every day is a new chance to become a better version of yourself.",
       "Small steps in the right direction become giant leaps over time.",
       "Dream big, start small, but most importantly, start.",
       "You don’t have to be great to start, but you have to start to be great.",
       "Push yourself because no one else is going to do it for you.",
-      "The harder you work for something, the greater you’ll feel when you achieve it."
+      "The harder you work for something, the greater you’ll feel when you achieve it.",
+      "Don’t wait for opportunity — create it."
     ]
   showRandomQuote() {
     const id = Math.floor(Math.random() * this.quotes.length);
-    return this.randomQuote.set(this.quotes[id]);
+    this.randomQuote.update(() => this.quotes[id]);
   }
 }
